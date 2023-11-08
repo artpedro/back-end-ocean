31ef90e684341cd0f5a53f9dda32df6ba1eba67d
Artur Pedro Martins Neto <87918748+artpedro@users.noreply.github.com>
2023-11-08 14:59:35 +0000

    "/oi/" end-point created

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log(1+2)
+// creating app
+const express = require('express')
+const app = express()
+const port = 3000
+
+// creating principal end-point using app
+// using method get
+// in the end-point /
+// with a response contain the string 'Hello World!'
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+// another end-point on /oi
+app.get('/oi', function (req, res) {
+    res.send('Olá Mundo')
+})
+
+// listen to port 3000 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
